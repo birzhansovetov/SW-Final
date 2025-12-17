e9c387396c6cc452c40d57d3b60909f813f5a814
Sovetov Birzhan <bsovetov301@gmail.com>
2025-12-17 17:07:46 +0500

    Add Student entity

---
+++ src/main/java/com/example/demo/Entity/Student.java
@@ -0,0 +1,22 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer age;
+}
+
