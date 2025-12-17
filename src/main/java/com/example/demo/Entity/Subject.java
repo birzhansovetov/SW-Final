9072a5c016b70240fb04ee0dee86c8fd87ab0663
Sovetov Birzhan <bsovetov301@gmail.com>
2025-12-17 17:26:47 +0500

    Add entities

---
+++ src/main/java/com/example/demo/Entity/Subject.java
@@ -0,0 +1,24 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "subjects")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "subjects")
+    private Set<Student> students;
+}
+
