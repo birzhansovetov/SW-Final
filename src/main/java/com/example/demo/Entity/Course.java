16c87dd98b6e423f5a69f1045ec08124bdf6abb9
Sovetov Birzhan <bsovetov301@gmail.com>
2025-12-19 02:33:10 +0500

    Add and fix entity mappings

---
+++ src/main/java/com/example/demo/Entity/Course.java
@@ -0,0 +1,24 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "course")
+    private List<Student> students;
+}
---
+++ src/main/java/com/example/demo/Entity/Subject.java
@@ -0,0 +1,25 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "subjects")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "subjects")
+    private List<Student> students;
+}
+
